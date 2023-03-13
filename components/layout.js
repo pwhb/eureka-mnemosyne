c8ac539515eb93f20f2b843919bb9aad676e2bb1
phonewaiyamhein <phonewaiyamhein@ayainnovation.com>
2023-03-13 16:14:15 +0630

    init

---
+++ components/layout.js
@@ -0,0 +1,10 @@
+import Footer from "./footer";
+import Navbar from "./navbar";
+
+export default function Layout({ children }) {
+    return <>
+        <Navbar />
+        {children}
+        <Footer />
+    </>
+}
